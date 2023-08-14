ce9a07c7bbd2384a48ec37a6331d853e33086384
Victor <victorobioratech@gmail.com>
2023-08-14 23:13:19 +0100

    summary section on main page initialized

---
+++ src/components/home/Summary/SummaryComponent.jsx
@@ -0,0 +1,13 @@
+import classes from './SummaryComponent.module.css'
+
+const SummaryComponent = props => {
+    return <section>
+        <div className={classes.summaryHeadings}>
+            <div><h1>ACTIVITY SUMMARY</h1></div>
+            <div><h1>SPENDING SUMMARY</h1></div>
+            <div><h1>INCOME SUMMARY</h1></div>
+        </div>
+    </section>
+};
+
+export default SummaryComponent
