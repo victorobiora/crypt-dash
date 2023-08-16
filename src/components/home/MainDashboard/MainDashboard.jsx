ea2f109a1c600996efc6a84abd264185ebd91f19
Victor <victorobioratech@gmail.com>
2023-08-16 19:31:57 +0100

    'summary component initiated'

--- src/components/home/MainDashboard/MainDashboard.jsx
+++ src/components/home/MainDashboard/MainDashboard.jsx
@@ -1,4 +1,5 @@
 import WalletOverview from "./WalletOverview";
+import SummaryComponent from "./Summary/SummaryComponent";
 import styles from "./WalletOverview.module.css";
 
 
@@ -6,6 +7,7 @@ const MainDashboard = (props) => {
   return (
     <div className={styles.container}>
       <WalletOverview />
+      <SummaryComponent/>
     </div>
   );
 };
--- src/components/home/MainDashboard/Summary/SummaryComponent.jsx
+++ src/components/home/MainDashboard/Summary/SummaryComponent.jsx
@@ -1,12 +1,13 @@
 import classes from './SummaryComponent.module.css'
 
 const SummaryComponent = props => {
-    return <section>
+    return <section className={classes.summaryContainer}>
         <div className={classes.summaryHeadings}>
             <div><h1>ACTIVITY SUMMARY</h1></div>
             <div><h1>SPENDING SUMMARY</h1></div>
             <div><h1>INCOME SUMMARY</h1></div>
         </div>
+        
     </section>
 };
 
