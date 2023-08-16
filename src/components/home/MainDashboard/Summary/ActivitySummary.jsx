346f390b14aeebe89240f86adeb490e1a4815973
Victor <victorobioratech@gmail.com>
2023-08-16 19:58:48 +0100

    'activity graph done'

--- src/components/home/MainDashboard/Summary/ActivitySummary.jsx
+++ src/components/home/MainDashboard/Summary/ActivitySummary.jsx
@@ -3,7 +3,15 @@ import classes from "./ActivitySummary.module.css";
 const ActivitySummary = (props) => {
   return (
     <section className={classes.summary_activity}>
-      <div className={classes.activity_line_chart}></div>
+      <div className={classes.activity_line_chart}>
+        <div className={classes.activity_heading}>
+            <p>Activity Graph</p>
+            <h4>$125k</h4>
+        </div>
+        <div>
+
+        </div>
+      </div>
       <div className={classes.activity_history}></div>
       <div className={classes.activity_search_Date}></div>
     </section>
