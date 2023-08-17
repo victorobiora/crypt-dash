27446f2ae3273073e06e12dfee763df2c4219887
Victor <victorobioratech@gmail.com>
2023-08-17 19:14:28 +0100

    'summary continued'

--- src/components/home/MainDashboard/Summary/ActivitySummary.jsx
+++ src/components/home/MainDashboard/Summary/ActivitySummary.jsx
@@ -5,15 +5,22 @@ const ActivitySummary = (props) => {
     <section className={classes.summary_activity}>
       <div className={classes.activity_line_chart}>
         <div className={classes.activity_heading}>
-            <p>Activity Graph</p>
-            <h4>$125k</h4>
+          <p>Activity Graph</p>
+          <h4>$125k</h4>
         </div>
-        <div>
-
+        <div></div>
+      </div>
+      <div className={classes.activity_history}>
+        <div className={classes.history_heading1}>
+          <h3>History</h3>
+        </div>
+        <div className={classes.history_heading2}>
+          <form>
+            <input type="date" />
+          </form>
+          <div className={classes.addToActivity}>+</div>
         </div>
       </div>
-      <div className={classes.activity_history}></div>
-      <div className={classes.activity_search_Date}></div>
     </section>
   );
 };
