f15822e5968c6c73d9f3a75a585ae651c20745b4
Victor <victorobioratech@gmail.com>
2023-08-20 00:21:20 +0100

    porfolio component 30% built

--- src/components/home/Portfolio/PortfolioComponent.jsx
+++ src/components/home/Portfolio/PortfolioComponent.jsx
@@ -1,7 +1,31 @@
-
+import classes from './PortfolioComponent.module.css'
+import { svgObject } from '../../../svg';
 
 const PortfolioComponent = props => {
-
+  return <section className={classes.portfolio_container}> 
+  <div className={classes.portfolio_heading}>
+    <h3>Kanonas Crypto</h3>
+    <p>Increase your profit</p>
+  </div>
+  <div className={classes.portfolio_display}>
+    <h4>My  Portfolio</h4>
+    <h2>$23,050.00</h2>
+    <div className={classes.portfolio_buttons}>
+        <button>
+            <div>
+        
+            </div>
+            Deposit
+        </button>
+        <button>
+        <div>
+               
+            </div>
+          Withdraw
+        </button>
+    </div>
+  </div>
+  </section>
 };
 
 
