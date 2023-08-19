1ff01f44b715a1a07c98fda3177986b137a1604a
Victor <victorobioratech@gmail.com>
2023-08-19 23:26:55 +0100

    portfolio component moved to home

--- src/components/home/HomeComponent.jsx
+++ src/components/home/HomeComponent.jsx
@@ -1,11 +1,13 @@
 import SideNav from "./SideNav";
 import MainDashboard from "./MainDashboard/MainDashboard";
+import PortfolioComponent from "./Portfolio/PortfolioComponent";
 
 const HomeComponent = (props) => {
   return (
     <section className="new_Page" style={{display: 'flex', flexDirection: 'row'}}>
       <SideNav />
       <MainDashboard/>
+      <PortfolioComponent/>
     </section>
   );
 };
