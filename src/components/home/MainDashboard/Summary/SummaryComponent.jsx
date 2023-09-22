d0c7f2c24e1ad5cb83abac162a6b5c4d66ce27d7
Victor <victorobioratech@gmail.com>
2023-09-22 23:29:07 +0100

    activity highlight added

--- src/components/home/MainDashboard/Summary/SummaryComponent.jsx
+++ src/components/home/MainDashboard/Summary/SummaryComponent.jsx
@@ -1,16 +1,28 @@
-import ActivitySummary from './ActivitySummary';
-import classes from './SummaryComponent.module.css'
+import ActivitySummary from "./ActivitySummary";
+import classes from "./SummaryComponent.module.css";
+import { useSelector } from "react-redux";
 
-const SummaryComponent = props => {
-    return <section className={classes.summaryContainer}>
-        <div className={classes.summaryHeadings}>
-            <div><h5>ACTIVITY SUMMARY</h5></div>
-            <div><h5>SPENDING SUMMARY</h5></div>
-            <div><h5>INCOME SUMMARY</h5></div>
+const SummaryComponent = () => {
+  const modeIsDay = useSelector((state) => state.mode.modeIsDay);
+  const modeClass = modeIsDay ? `dayBackground` : `nightBackground`
+
+
+  return (
+    <section className={classes.summaryContainer}>
+      <div className={classes.summaryHeadings}>
+        <div id={modeClass}>
+          <h5>ACTIVITY SUMMARY</h5>
         </div>
-        <ActivitySummary/>
-        
+        <div>
+          <h5>SPENDING SUMMARY</h5>
+        </div>
+        <div>
+          <h5>INCOME SUMMARY</h5>
+        </div>
+      </div>
+      <ActivitySummary />
     </section>
+  );
 };
 
-export default SummaryComponent
+export default SummaryComponent;
